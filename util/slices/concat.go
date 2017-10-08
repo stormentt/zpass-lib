8298f1ca3c490457ac836d82e4dcf9d849001baf
Tanner Storment <tanner@secrecy.email>
2017-10-08 00:26:39 -0500

    Added slices util: combine()

---
+++ util/slices/concat.go
@@ -0,0 +1,20 @@
+package slices
+
+// combine combines all the byte slices into one big byte slice
+func combine(slices ...[]byte) []byte {
+	var length int
+	for _, slice := range slices {
+		length += len(slice)
+	}
+
+	// Pre allocating an array & using copy() is quicker than using append() because append() creates a bunch of new arrays each time it runs out of room
+	// Pre allocating also uses less memory
+	tmp := make([]byte, length)
+
+	var pos int
+	for _, slice := range slices {
+		pos += copy(tmp[pos:], slice)
+	}
+
+	return tmp
+}
