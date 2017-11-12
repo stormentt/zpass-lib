72bb83001c49c36d38b4879abae9f8829fb5a0fe
Tanner Storment <tanner@secrecy.email>
2017-11-12 17:13:16 -0600

    askPass now returns a []byte instead of string

--- util/askPass.go
+++ util/askPass.go
@@ -2,16 +2,17 @@ package util
 
 import (
 	"fmt"
+
 	"golang.org/x/crypto/ssh/terminal"
 )
 
 //AskPass prints the prompt to the screen & then reads input
-func AskPass(prompt string) (string, error) {
+func AskPass(prompt string) ([]byte, error) {
 	fmt.Print(prompt)
 	input, err := terminal.ReadPassword(0)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	fmt.Println("")
-	return string(input), nil
+	return input, nil
 }
