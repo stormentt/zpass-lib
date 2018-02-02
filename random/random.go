fe47e855d42dab30c2ed21d9fc0ee85cf9cfca1e
Tanner Storment <tanner@secrecy.email>
2018-02-01 22:57:40 -0600

    refactoring

--- random/random.go
+++ random/random.go
@@ -5,23 +5,24 @@ import (
 	"crypto/rand"
 	"math/big"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/stormentt/zpass-lib/util"
 )
 
+func Check() error {
+	b := make([]byte, 256)
+	_, err := rand.Read(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Bytes returns a random byte slice of length n
-// If there's an error generating the bytes it'll return (nil, error)
 func Bytes(n int) ([]byte, error) {
 	b := make([]byte, n)
 	_, err := rand.Read(b)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-		}).Debug("Error generating random bytes")
-		return nil, err
-	}
-
-	return b, nil
+	return b, err
 }
 
 //ByteB64 returns a base64 encoded string containing n random bytes
@@ -30,7 +31,6 @@ func BytesB64(n int) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	str := util.EncodeB64(bytes)
 	return str, nil
 }
@@ -40,26 +40,22 @@ func Int(max int64) (int64, error) {
 	bigMax := big.NewInt(max)
 	randInt, err := rand.Int(rand.Reader, bigMax)
 	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-		}).Debug("Error generating random integer")
 		return 0, err
 	}
-
 	return randInt.Int64(), nil
 }
 
 // AlphaNum returns a mixed-case alphanumeric string of the specified length
 func AlphaNum(length int) (string, error) {
 	alphanumeric := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	alphaLen := int64(len(alphanumeric))
 
 	ret := make([]byte, length)
 	for i := 0; i < length; i++ {
-		letter, err := Int(int64(len(alphanumeric)))
+		letter, err := Int(alphaLen)
 		if err != nil {
 			return "", err
 		}
-
 		ret[i] = alphanumeric[letter]
 	}
 
