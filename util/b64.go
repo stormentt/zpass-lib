cd853e16b4cc46187c98e94a8d45d197383385c6
Tanner Storment <tanner@secrecy.email>
2018-02-16 11:00:54 -0600

    Removed logrus

--- util/b64.go
+++ util/b64.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"encoding/base64"
-	log "github.com/sirupsen/logrus"
 )
 
 //EncodeB64 encodes the given bytes into a base64 string
@@ -12,13 +11,5 @@ func EncodeB64(bytes []byte) string {
 
 //DecodeB64 decodes a base64 string into bytes
 func DecodeB64(message string) ([]byte, error) {
-	bytes, err := base64.StdEncoding.DecodeString(message)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"String": message,
-			"Error":  err,
-		}).Debug("Unable to decode base64 string")
-		return nil, err
-	}
-	return bytes, nil
+	return base64.StdEncoding.DecodeString(message)
 }
